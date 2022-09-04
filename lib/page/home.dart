27e6aa5366bd27a0e005b09c654fa3a4d1d07021
algosketch <sjy_9484@naver.com>
2022-09-04 19:03:47 +0900

    ADD: single item ui in list

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:naver_map_plugin/naver_map_plugin.dart';
+
+import '../common/ChColors.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,24 +18,117 @@ class _HomeState extends State<Home> {
   MapType _mapType = MapType.Basic;
 
   void _showList(BuildContext context) {
-    showBottomSheet(context: context, builder: (context) {
-      return Container(
-        height: 200,
-        width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.white
+    showBottomSheet(
+        context: context,
+        builder: (context) {
+          return Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(12)),
+              child: _listItem());
+        });
+  }
+
+  Widget _listItem() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 24),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    'https://picsum.photos/360',
+                    width: MediaQuery.of(context).size.width / 2 - 21,
+                    height: MediaQuery.of(context).size.width / 2 - 21,
+                  ),
+                ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.network(
+                    'https://picsum.photos/360',
+                    width: MediaQuery.of(context).size.width / 2 - 21,
+                    height: MediaQuery.of(context).size.width / 2 - 21,
+                  ),
+                ),
+              ],
+            ),
           ),
-          child: Text("바텀~~~ 시트~~~"));
-    });
+          Padding(
+              padding: EdgeInsets.only(top: 16, bottom: 8),
+              child: Text(
+                "미스터디유커피",
+                style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              )),
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 8),
+                child: SvgPicture.asset(
+                  'assets/marker_outlined.svg',
+                  color: ChColors.black,
+                  width: 16,
+                  height: 16,
+                ),
+              ),
+              Text("인천 연수구 아카데미로 119")
+            ],
+          ),
+          Padding(padding: EdgeInsets.only(bottom: 8)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 8),
+                      child: SvgPicture.asset(
+                        'assets/time.svg',
+                        color: ChColors.black,
+                        width: 16,
+                        height: 16,
+                      ),
+                    ),
+                    Text("10:00 ~ 17:30")
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 8),
+                      child: SvgPicture.asset(
+                        'assets/power.svg',
+                        color: ChColors.black,
+                        width: 16,
+                        height: 16,
+                      ),
+                    ),
+                    Text("콘센트 많음")
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-            children: [
+        child: Stack(alignment: Alignment.bottomCenter, children: [
           NaverMap(
             initLocationTrackingMode: LocationTrackingMode.Follow,
             locationButtonEnable: true,
@@ -40,25 +136,20 @@ class _HomeState extends State<Home> {
             mapType: _mapType,
           ),
           Container(
-            alignment: Alignment.center,
-            width: 100,
-            height: 32,
-            margin: EdgeInsets.only(bottom: 24),
+              alignment: Alignment.center,
+              width: 100,
+              height: 32,
+              margin: EdgeInsets.only(bottom: 24),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16)
-              ),
-              child: Builder(
-                builder: (context) {
-                  return TextButton(
-                    child: Text("목록 보기"),
-                    onPressed: () {
-                      _showList(context);
-                    },
-                  );
-                }
-              )
-          )
+                  color: Colors.white, borderRadius: BorderRadius.circular(16)),
+              child: Builder(builder: (context) {
+                return TextButton(
+                  child: Text("목록 보기"),
+                  onPressed: () {
+                    _showList(context);
+                  },
+                );
+              }))
         ]),
       ),
     );
