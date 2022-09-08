e1b7dd221a4e3a786148da6a0976119436606336
altpfwlzh <altpfwlzh@gmail.com>
2022-09-08 18:10:16 +0900

    ADD : CustomMarker class

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -6,11 +6,18 @@ class HomeController extends getx.GetxController {
   var bottomSheetVisibility = false.obs;
   List<Marker> markers = [
     Marker(
-      markerId: DateTime.now().toIso8601String(),
-      position: LatLng(37.563600, 126.962370),
-      infoWindow: '테스트'
+        markerId: DateTime.now().toIso8601String(),
+        position: LatLng(37.5145162, 127.0254446),
+        infoWindow: '수목금토 카페'
+    ),
+    Marker(
+        markerId: DateTime.now().toIso8601String(),
+        position: LatLng(37.5265687, 127.0359859),
+        infoWindow: '우디집 도산점'
     )
   ];
+
+
 
   var cafes = [
     CafeInfo('미스터디유커피1', '인천 연수구 아카데미로 119', '10:30 ~ 17:30', '콘센트 많음'),
