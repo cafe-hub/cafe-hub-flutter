dce2fb048459b67af13aa7d33764a2f4daa0cc04
algosketch <sjy_9484@naver.com>
2022-09-17 17:05:33 +0900

    ADD: 지도에 표시되는 마커 변경

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -6,13 +6,23 @@ import 'package:cafe_hub_flutter/service/cafe_service.dart';
 class HomeController extends GetxController {
   var bottomSheetVisibility = false.obs;
   var cafes = <CafeInfo>[].obs;
+  OverlayImage? overlayImage;
+
+  HomeController() {
+    _setOverlayImage();
+  }
+
+  _setOverlayImage() async {
+    overlayImage = await OverlayImage.fromAssetImage(assetName: "assets/marker.png");
+  }
 
   List<Marker> getMarkers(void Function(Marker? marker, Map<String, int?> iconSize) action) {
     return cafes.map(
             (cafeInfo) => Marker(
             markerId: cafeInfo.id,
             position: cafeInfo.latLng,
-            onMarkerTab: action,)
+            onMarkerTab: action,
+            icon: overlayImage)
     ).toList();
   }
 
